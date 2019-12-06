ac9222c22bada8ac447ddf09521c091ed3bb7dfc
wwe221 <wwe22123@gmail.com>
2019-12-06 17:00:29 +0900

    this

--- crawling/views.py
+++ crawling/views.py
@@ -45,19 +45,8 @@ def allHTML(request):
     }
     return render(request, 'twitch.html',context)
 def getbysele():
-    url = 'https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig'
-    path ='C:/chromedriver'
-    options = webdriver.ChromeOptions()
-    browser = webdriver.Chrome(path,chrome_options=options)
-    browser.get(url)
-    html = browser.page_source
-    browser.quit()
-    soup = BeautifulSoup(html,'html.parser')
-    primary = soup.select_one('.contents')
-    # item_sctions = primary.select('.ytd-item-section-renderers')
-    # for tmp in item_sctions:
-    #     print(tmp)
-    return soup
+    
+    return 
 def getYoutube():
     url = 'https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig'    
     data = requests.get(url).text
@@ -92,7 +81,7 @@ def getYoutube():
         a = 0
         for tp in cma[0].split(','):
             a *=1000
-            a =10
+            a += int(tp)
         v = a
         if Stream.objects.filter(channel_name=channel).exists():
             stream = Stream.objects.get(channel_name=channel)
@@ -166,62 +155,7 @@ def getTwitch():
             tmp.delete()
     print("getTwitch done")
     return
-def getAfreeca():
-    url = "http://www.afreecatv.com/"
-    path ='C:/chromedriver'
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-    browser = webdriver.Chrome(path,chrome_options=options)
-    browser.get(url)
-    g = browser.find_elements_by_css_selector('.onAir')
-    html = browser.page_source
-    browser.quit()
-    soup = BeautifulSoup(html,'html.parser')
-    onAir = soup.select_one('.onAir')
-    lists = onAir.select('li')
-    lives=[]
-    length=len(lists)
-    for tmp in lists:
-        title = tmp.select_one('.subject').text
-        v = tmp.select_one('.viewer').text.split(' ')
-        vs = v[0]
-        thumbnail = tmp.select_one('.thumb img')['src']
-        channel = tmp.select_one('.nick').text
-        link = tmp.select_one('.box_link')['href']
-        text_over_flag= 0    
-        if len(title) > 20:
-            text_over_flag= 1
-        if vs[0:2] =='시작':
-            continue
-        if vs[0:2] =='조회':            
-            continue
-        cma = vs.split('명')
-        a = 0
-        for tp in cma[0].split(','):
-            a *=1000
-            a += int(tp)
-        v = a
-        if Stream.objects.filter(channel_name=channel).exists():
-            stream = Stream.objects.get(channel_name=channel)
-        else:
-            stream = Stream()                        
-        stream.channel_name = channel
-        stream.title = title
-        stream.stream_url = link
-        stream.stream_embed_url = link
-        stream.stream_views = v
-        stream.stream_thumbnail = thumbnail
-        stream.platform = 2
-        stream.tof=text_over_flag
-        stream.save()
-        lives.append(channel)
-    before = Stream.objects.filter(platform=2)
-    for tmp in before:
-        t = tmp.channel_name
-        if not t in lives:
-            tmp.delete()
+def getAfreeca():    
     print("getAfreeca done")
     return
 
