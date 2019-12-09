659d845a0ddffd6295e82fadc556a07022d3debf
wwe221 <wwe22123@gmail.com>
2019-12-09 10:40:40 +0900

    del chg

--- hotplay/settings.py
+++ hotplay/settings.py
@@ -9,10 +9,6 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hotplay.settings")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
