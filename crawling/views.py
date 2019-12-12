6f06c57113ab4a4b14cefcbd3716773e3f088ef2
wwe221 <wwe22123@gmail.com>
2019-12-12 16:39:30 +0900

    asdf

--- crawling/views.py
+++ crawling/views.py
@@ -171,7 +171,7 @@ def getAfreeca():
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.binary_location = GOOGLE_CHROME_PATH
-    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=options)
     browser.get(url)
     g = browser.find_elements_by_css_selector('.onAir')
     html = browser.page_source
