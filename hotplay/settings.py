87fa075f7eeacdd3647bc79d144d7491da3d5e96
wwe221 <wwe22123@gmail.com>
2019-12-12 11:06:18 +0900

    add host

--- hotplay/settings.py
+++ hotplay/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#7)9edgh+zlt#%3ts=!i$vt85*y)!!_k!j_0w26e*&9hy-9843'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hotplay.herokuapp.com/','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['https://hotplay.herokuapp.com/','localhost','127.0.0.1','hotplay.herokuapp.com']
 
 
 # Application definition
