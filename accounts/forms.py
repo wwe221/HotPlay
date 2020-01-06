d2601ddc0aefceb3a2d844d9afc196a2ce811d82
bookkg2 <bookkg2@naver.com>
2020-01-06 16:59:31 +0900

    email test check

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,13 +3,18 @@ from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from .models import User
 from django.conf import settings
 class CustomUserCreateionForm(UserCreationForm):
-    nickname = forms.CharField(min_length=3)    
+    nickname = forms.CharField(min_length=3)
+    
     class Meta(UserCreationForm.Meta):
+        
         model = User
-        fields = UserCreationForm.Meta.fields + ('nickname',)
+        fields = ['username', 'email', 'password']
         # 기존에 있던 field 에 하나더 추가 한다는 말.
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta(AuthenticationForm):
         model = User
+        
+
+
 
